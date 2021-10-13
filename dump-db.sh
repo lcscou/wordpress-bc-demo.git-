1f7b91c3a40a3f13f45c44d770fc8166d94a5167
Lucas Coutinho <lcscou@gmail.com>
2021-10-13 10:57:56 -0300

    Creates a dump of database

--- dump-db.sh
+++ dump-db.sh
@@ -1,1 +1,5 @@
-docker exec -i perimetre/wordpress mysqldump -h mysql -u wordpress -pwordpress --add-drop-table wordpress | gzip > "./db/dump/wordpress.sql.zip"
+if [ ! -d "./db-dump" ]; then
+    mkdir dump-db
+fi
+
+docker exec -i wordpress-bc-demo_phpmyadmin_1 mysqldump -h mysql -u wordpress -pwordpress --add-drop-table wordpress | gzip > "./db-dump/wordpress.sql.zip"
