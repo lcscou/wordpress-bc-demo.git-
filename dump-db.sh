078cf7b0f4fc2ac933ae2304146378f46109a2a2
Lucas Coutinho <lcscou@gmail.com>
2021-10-13 11:02:50 -0300

    Remove Script

--- dump-db.sh
+++
@@ -1,5 +0,0 @@
-if [ ! -d "./db-dump" ]; then
-    mkdir dump-db
-fi
-
-docker exec -i wordpress-bc-demo_phpmyadmin_1 mysqldump -h mysql -u wordpress -pwordpress --add-drop-table wordpress | gzip > "./db-dump/wordpress.sql.zip"
